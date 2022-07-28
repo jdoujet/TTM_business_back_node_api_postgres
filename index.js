4bd11e017e7a58337ce95b206096bf6c03c46c96
Julien DOUJET <jdoujet@gmail.com>
2022-07-28 07:47:06 +0200

    heroku port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = process.env.PORT //35355 by default for my heroku
 const db = require('./queries')
 
 app.use(bodyParser.json())
