efaf86593e5f9dc9ddfddf045281fab8ff8646ca
Julien DOUJET <jdoujet@gmail.com>
2022-07-28 07:30:25 +0200

    init server

---
+++ queries.js
@@ -0,0 +1,19 @@
+const Pool = require('pg').Pool
+const connectionString = process.env.DATABASE_URL
+const pool = new Pool({
+  connectionString,
+})
+
+
+const getUsers = (request, response) => {
+    pool.query('SELECT * FROM utilisateurs', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+
+  module.exports = {
+    getUsers
+  }
