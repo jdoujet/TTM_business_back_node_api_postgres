5a7e15023f63ddc2e6000ba751047b0241c0c9a6
Julien DOUJET <jdoujet@gmail.com>
2022-09-04 12:56:11 +0200

    getArticleByName

--- queries.js
+++ queries.js
@@ -51,7 +51,7 @@ const getArticleById = (request, response) => {
 
 const getArticleByName = (request, response) => {
   const nom_article = request.params.nom_article
-  pool.query("SELECT * FROM article WHERE nom_article LIKE $1 ;", ["'%"+nom_article+"%'"], (error, results) => {
+  pool.query("SELECT * FROM article WHERE nom_article LIKE $1 ;", ['%pomme%'], (error, results) => {
     if (error) {
       throw error
     }
